16de5f7b71fe83247ebd1e21def19ec095cfecb5
Andrii Kurdiumov <kant2002@gmail.com>
2024-10-28 10:35:59 +0500

    Add selection of dresses

--- dist/index.js
+++ dist/index.js
@@ -73,31 +73,75 @@ function dressGame() {
             };
         }
         yield Promise.all(promises);
-        context.drawImage("Девочка");
-        //context.drawImage("Сумочка");
-        context.drawThumb("Сумочка", 200, 0);
-        //context.drawImage("Pasted Layer #1");
-        context.drawThumb("Pasted Layer #1", 200, 20);
-        //context.drawImage("Футболка 1");
-        context.drawThumb("Футболка 1", 300, 20);
-        //context.drawImage("Блузка 1");
-        context.drawThumb("Блузка 1", 200, 80);
-        //context.drawImage("юбка 1");
-        context.drawThumb("юбка 1", 200, 150);
-        //context.drawImage("юбка 2");
-        context.drawThumb("юбка 2", 300, 150);
-        //context.drawImage("Брюки 1");
-        context.drawThumb("Брюки 1", 200, 190);
-        //context.drawImage("Туфли 1");
-        context.drawThumb("Туфли 1", 200, 320);
-        //context.drawImage("Туфли 2");
-        context.drawThumb("Туфли 2", 320, 320);
-        //context.drawImage("Перчатки");
-        context.drawThumb("Перчатки", 300, 80);
-        //context.drawImage("Вечернее платье");
-        context.drawThumb("Вечернее платье", 250, 190);
-        //context.drawImage("Пальто");
-        context.drawThumb("Пальто", 320, 190);
+        const одежда = {
+            аксессуары: null,
+            топы: null,
+            юбки: null,
+            туфли: null,
+            крупнаяОдежда: null,
+        };
+        var аксессуары = { название: "аксессуары", высота: 20, ширина: 100, наборы: ["Сумочка"] };
+        var топы = { название: "топы", высота: 60, ширина: 100, наборы: ["Pasted Layer #1", "Футболка 1", "Блузка 1", "Перчатки"] };
+        var юбки = { название: "юбки", высота: 40, ширина: 100, наборы: ["юбка 1", "юбка 2"] };
+        var крупнаяодежда = { название: "крупнаяОдежда", высота: 130, ширина: 60, наборы: ["Брюки 1", "Вечернее платье", "Пальто"] };
+        var туфли = { название: "туфли", высота: 40, ширина: 115, наборы: ["Туфли 1", "Туфли 2"] };
+        var панельАксессуаров = [аксессуары, топы, юбки, крупнаяодежда, туфли];
+        var блоки = [];
+        let начальнаяВысота = 0;
+        for (const аксессуары of панельАксессуаров) {
+            let начальнаяПозиция = 0;
+            for (const аксессуар of аксессуары.наборы) {
+                //context.drawThumb(аксессуар, 200 + начальнаяПозиция, начальнаяВысота);
+                блоки.push({ группа: аксессуары.название, аксессуар, х: 200 + начальнаяПозиция, у: начальнаяВысота, ширина: аксессуары.ширина, высота: аксессуары.высота });
+                начальнаяПозиция += аксессуары.ширина;
+                if (начальнаяПозиция >= 200) {
+                    начальнаяПозиция = 0;
+                    начальнаяВысота += аксессуары.высота;
+                }
+            }
+            if (начальнаяПозиция !== 0) {
+                начальнаяВысота += аксессуары.высота;
+            }
+        }
+        c.addEventListener("click", function (ev) {
+            //console.log(ev.clientX, ev.clientY, ev);
+            var выбранныеБлоки = блоки.filter(i => (ev.clientX >= i.х && ev.clientX <= i.х + i.ширина)
+                && (ev.clientY >= i.у && ev.clientY <= i.у + i.высота));
+            if (выбранныеБлоки.length > 0) {
+                if (одежда[выбранныеБлоки[0].группа] === выбранныеБлоки[0].аксессуар) {
+                    одежда[выбранныеБлоки[0].группа] = null;
+                }
+                else {
+                    одежда[выбранныеБлоки[0].группа] = выбранныеБлоки[0].аксессуар;
+                }
+            }
+            drawAll();
+        });
+        function drawAll() {
+            context.clear();
+            блоки.forEach(элемент => {
+                context.drawThumb(элемент.аксессуар, элемент.х, элемент.у);
+            });
+            context.drawImage("Девочка");
+            for (const группа of Object.keys(одежда)) {
+                if (одежда[группа]) {
+                    context.drawImage(одежда[группа]);
+                }
+            }
+        }
+        drawAll();
+        // context.drawThumb("Сумочка", 200, 0);
+        // context.drawThumb("Pasted Layer #1", 200, 20);
+        // context.drawThumb("Футболка 1", 300, 20);
+        // context.drawThumb("Блузка 1", 200, 80);
+        // context.drawThumb("Перчатки", 300, 80);
+        // context.drawThumb("юбка 1", 200, 150);
+        // context.drawThumb("юбка 2", 300, 150);
+        // context.drawThumb("Брюки 1", 200, 190);
+        // context.drawThumb("Вечернее платье", 250, 190);
+        // context.drawThumb("Пальто", 320, 190);
+        // context.drawThumb("Туфли 1", 200, 320);
+        // context.drawThumb("Туфли 2", 320, 320);
     });
 }
 document.addEventListener("DOMContentLoaded", function () {
